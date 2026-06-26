fb319b60c659879115fba5402e7744ea8fd685ba
Fikret Efe Erdem <eefe922@gmail.com>
2026-06-26 16:50:00 +0300

    26.06.26 DersSonu

--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -3,9 +3,6 @@
 MyList<string> isimler = new MyList<string>();
 isimler.Add("Engin");
 
-List<string> liste = new List<string>();
-Console.WriteLine(liste.Count);
-
 
 Console.WriteLine("Hello, World!");
 
---
+++ OOP3/Program.cs
@@ -0,0 +1,17 @@
+﻿using OOP3;
+
+IhtiyacKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+TasitKrediManager tasitKrediManager = new TasitKrediManager();
+KonutKrediManager konutKrediManager = new KonutKrediManager();
+
+ILoggerService databaseLoggerService = new DatabaseLoggerService();
+ILoggerService FileLoggerService = new FileLoggerService();
+
+
+BasvuruManager basvuruManager = new BasvuruManager();
+basvuruManager.BasvuruYap(ihtiyacKrediManager, new DatabaseLoggerService());
+
+List<IKrediManager> krediler = new List<IKrediManager>{ihtiyacKrediManager, tasitKrediManager};
+{ };
+
+//basvuruManager.KrediOnBilgilendirmesiYap(krediler);
